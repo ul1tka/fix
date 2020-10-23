36d7acce398e7ddc9267530ce9293613a52a8906
Vlad Lazarenko <vlad@lazarenko.me>
2020-10-22 22:39:41 -0400

    Simplify header prep code

--- src/store/header.cc
+++ src/store/header.cc
@@ -20,58 +20,32 @@
 
 namespace fix {
 
+static std::vector<std::byte>&
+operator<<(std::vector<std::byte>& dst, std::string_view src)
+{
+    auto p = reinterpret_cast<const std::byte*>(src.data());
+    dst.insert(dst.end(), p, p + src.size());
+    return dst;
+}
+
 header::header(
     std::string_view proto,
     std::string_view sender,
     std::string_view target
 ) noexcept {
-    const auto total_size =
-        proto.size() + sender.size() + target.size() +
-        max_body_length_digits() + 21;
-
+    const auto total_size = (proto.size() + sender.size() + target.size() +
+                             max_body_length_digits() + 21);
     data_.reserve(total_size);
-
-    data_.emplace_back(std::byte{'8'});
-    data_.emplace_back(std::byte{'='});
-
+    data_ << "8=";
     for (const auto c : proto)
         data_.emplace_back(std::byte(c));
-
-    data_.emplace_back(std::byte{'\1'});
-    data_.emplace_back(std::byte{'9'});
-    data_.emplace_back(std::byte{'='});
-
+    data_ << "\0019=";
     for (unsigned int i = 0; i < max_body_length_digits(); ++i)
         data_.emplace_back(std::byte{' '});
-
-    data_.emplace_back(std::byte{'\1'});
-    data_.emplace_back(std::byte{'3'});
-    data_.emplace_back(std::byte{'5'});
-    data_.emplace_back(std::byte{'='});
-
+    data_ << "\00135=";
     begin_off_ = static_cast<unsigned int>(data_.size());
     data_.emplace_back(std::byte{' '});
-
-    data_.emplace_back(std::byte{'\1'});
-    data_.emplace_back(std::byte{'4'});
-    data_.emplace_back(std::byte{'9'});
-    data_.emplace_back(std::byte{'='});
-
-    for (const auto c : sender)
-        data_.emplace_back(std::byte(c));
-
-    data_.emplace_back(std::byte{'\1'});
-    data_.emplace_back(std::byte{'5'});
-    data_.emplace_back(std::byte{'6'});
-    data_.emplace_back(std::byte{'='});
-
-    for (const auto c : target)
-        data_.emplace_back(std::byte(c));
-
-    data_.emplace_back(std::byte{'\1'});
-    data_.emplace_back(std::byte{'3'});
-    data_.emplace_back(std::byte{'4'});
-    data_.emplace_back(std::byte{'='});
+    data_ << "\00149=" << sender << "\00156=" << target << "\00134=";
 }
 
 void header::append(std::string_view data)
@@ -80,9 +54,7 @@ void header::append(std::string_view data)
     data_.resize(data_.size() - 3);
     auto p = reinterpret_cast<const std::byte*>(data.data());
     data_.insert(data_.end(), p, p + data.size());
-    data_.emplace_back(std::byte{'3'});
-    data_.emplace_back(std::byte{'4'});
-    data_.emplace_back(std::byte{'='});
+    data_ << "34=";
 }
 
 } // namespace fix
